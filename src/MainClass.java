c09663df227fd2ad5580cedcc870d26a0f5cb247
KomodoFactory <github@komodofactory.com>
2016-12-09 17:44:28 +0100

    main loop created

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,70 +1,98 @@
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
- 
+
 import lejos.robotics.navigation.DifferentialPilot;
- 
+
 import lejos.nxt.TouchSensor;
 import lejos.nxt.LightSensor;
 import lejos.nxt.UltrasonicSensor;
- 
+
 public class MainClass {
- 
-    DifferentialPilot pilot;
-    TouchSensor bump = new TouchSensor(SensorPort.S1);
-    LightSensor light = new LightSensor(SensorPort.S2);
-    UltrasonicSensor ultra = new UltrasonicSensor(SensorPort.S3);
-   
-    public void go(){
-        pilot.travel(20000,true);
-        while(pilot.isMoving()){
-            //System.out.println("Light Value: "+light.readValue());
-            int dis = ultra.getDistance();
-            if(dis < 255){
-                pilot.travel(dis,true);
-                //TODO greifen, return to base
-            }
-            if(light.readValue() > 30){
-                pilot.travel(-15);
-                pilot.rotate(120.0);
-                this.go();
-            }  
-            if (bump.isPressed()) pilot.stop();
-        }
-       
-        System.out.println("Distance: "+pilot.getMovement().getDistanceTraveled());
-       
-        Button.waitForAnyPress();
-    }
-   
-    public static void main(String[] args) {
-               
-        //LightSensor light = new LightSensor(SensorPort.S2);
-        //System.out.println("Light Value: "+light.readNormalizedValue());
-        //Button.waitForAnyPress();
-       
-        MainClass traveler = new MainClass();
-        traveler.pilot = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.B);
-        traveler.go();
-    }
-   
-    public int drehen(double grad){ 
-    	grad = grad/5.0;
-    	double deg;
-    	if(grad > 0){
-    		deg = 5.0;
-    	}else{
-    		deg = -5.0;
-    	}
-    	
-    	for(int i = 0; i < grad; i++){
-    		int dist = ultra.getDistance();
-    		if(dist < 255) {
-    			return dist;
-    		}else{
-    			pilot.rotate(deg);
-    		}
-    	}
-    	return 255;
-    }
+
+	DifferentialPilot controllUnit;
+	TouchSensor emergencyButton = new TouchSensor(SensorPort.S1);
+	LightSensor lightSensor = new LightSensor(SensorPort.S2);
+	UltrasonicSensor ultraSensor = new UltrasonicSensor(SensorPort.S3);
+
+	int distanceBasedOnUnltrasonic;
+	boolean mainLoopCondition;
+	boolean greifarmOffen = true;
+
+	int noResultOnUltrasonic = 255;
+	int whiteTapeLightReading = 30;
+
+	public void mainLoop() {
+		while (mainLoopCondition) {
+			controllUnit.travel(50, true);
+
+			if (lightSensor.readValue() >= whiteTapeLightReading) {
+				controllUnit.travel(-15);
+				drehenMitSuchfunktion(120);
+			} else {
+				distanceBasedOnUnltrasonic = ultraSensor.getDistance();
+				if (distanceBasedOnUnltrasonic != noResultOnUltrasonic) {
+					gefundenesObjektAnsteuerungUndGreifen();
+					mainLoopCondition = false; 
+					//TODO: dont do this later  und returnToBase;
+				}
+			}
+
+			if (emergencyButton.isPressed()) {
+				controllUnit.quickStop();
+				mainLoopCondition = false;
+			}
+		}
+		
+		System.out.println("Distance: " + controllUnit.getMovement().getDistanceTraveled());
+
+		Button.waitForAnyPress();
+	}
+
+	public static void main(String[] args) {
+		MainClass traveler = new MainClass();
+		traveler.controllUnit = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.B);
+		traveler.mainLoopCondition = true;
+		traveler.mainLoop();
+		
+		//TODO: waypoint base
+	}
+
+	public int drehenMitSuchfunktion(double grad) {
+		double deg;
+		if (grad > 0) {
+			deg = 5.0;
+		} else {
+			deg = -5.0;
+		}
+
+		for (int i = 0; i < grad;) {
+			controllUnit.rotate(deg);
+			distanceBasedOnUnltrasonic = ultraSensor.getDistance();
+			if (distanceBasedOnUnltrasonic < 255) {
+				return distanceBasedOnUnltrasonic;
+			}
+			i += 5;
+		}
+		return 255;
+	}
+
+	public void gefundenesObjektAnsteuerungUndGreifen() {
+		controllUnit.travel(distanceBasedOnUnltrasonic, true);
+		schließeGreifarm();
+	}
+
+	public void schließeGreifarm() {
+		if (greifarmOffen) {
+			Motor.C.rotate(-90);
+			greifarmOffen = false;
+		}
+	}
+
+	public void offneGreifarm() {
+		if (!greifarmOffen) {
+			Motor.C.rotate(90);
+			greifarmOffen = true;
+		}
+	}
 }
