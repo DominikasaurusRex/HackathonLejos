65e25ee53adf90016f6b617c2633c5c2906e4d38
Thomas <thomas.knaub98@gmail.com>
2016-12-09 16:52:07 +0100

    test

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,10 +1,51 @@
 import lejos.nxt.Button;
-
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+ 
+import lejos.robotics.navigation.DifferentialPilot;
+ 
+import lejos.nxt.TouchSensor;
+import lejos.nxt.LightSensor;
+import lejos.nxt.UltrasonicSensor;
+ 
 public class MainClass {
-
-	public static void main(String[] args) {
-		System.out.println("first try");
-		Button.waitForAnyPress();
-	}
-	
+ 
+    DifferentialPilot pilot;
+    TouchSensor bump = new TouchSensor(SensorPort.S1);
+    LightSensor light = new LightSensor(SensorPort.S2);
+    UltrasonicSensor ultra = new UltrasonicSensor(SensorPort.S3);
+   
+    public void go(){
+        pilot.travel(20000,true);
+        while(pilot.isMoving()){
+            //System.out.println("Light Value: "+light.readValue());
+            int dis = ultra.getDistance();
+            if(dis < 255){
+                pilot.travel(dis,true);
+                //TODO greifen, return to base
+            }
+            if(light.readValue() > 30){
+                pilot.travel(-15);
+                pilot.rotate(120.0);
+                this.go();
+            }  
+            if (bump.isPressed()) pilot.stop();
+        }
+       
+        System.out.println("Distance: "+pilot.getMovement().getDistanceTraveled());
+       
+        Button.waitForAnyPress();
+    }
+   
+    public static void main(String[] args) {
+               
+        //LightSensor light = new LightSensor(SensorPort.S2);
+        //System.out.println("Light Value: "+light.readNormalizedValue());
+        //Button.waitForAnyPress();
+       
+        MainClass traveler = new MainClass();
+        traveler.pilot = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.B);
+        traveler.go();
+    }
+   
 }
